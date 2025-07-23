d1db69bc87db33e3a04e1c424bbc5dac370f6a35
kamaukimani <paulkimys285@gmail.com>
2025-07-23 10:21:42 +0300

    add more words in the dictionary

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ function isWordValid(word) {
     'HELLO', 'WORLD', 'REACT', 'SCRABBLE', 'CODE', 'FIGMA', 'TILE', 'RACK', 'BOARD',
     'SCORE', 'TURN', 'GAME', 'JAVASCRIPT', 'PYTHON', 'FRONTEND', 'BACKEND',
     'DEVELOPER', 'FUNCTION', 'VARIABLE', 'BUTTON', 'INPUT', 'TIMER', 'PLAYER',
-    'MOVE', 'WORD'
+    'MOVE', 'WORD','ABOUT', 'ABOVE', 'ACT', 'ADD', 'AFTER', 'AGAIN', 'AIR', 'ALL', 'ALMOST', 'ALONG',
+  'ALSO', 'ALWAYS', 'AM', 'AMONG', 'AN', 'AND', 'ANIMAL', 'ANOTHER', 'ANSWER', 'ANY',
+  'ARE', 'AROUND', 'AS', 'ASK', 'AT', 'AWAY', 'BACK', 'BALL', 'BASE', 'BE', 'BECAUSE',
+  'BEEN', 'BEFORE', 'BEGAN', 'BEGIN', 'BEHIND', 'BEING', 'BELL', 'BEST', 'BETTER',
+  'BETWEEN', 'BIG', 'BIRD', 'BLACK', 'BLUE', 'BOAT', 'BODY', 'BOOK', 'BOTH', 'BOY',
+  'BRING', 'BROUGHT', 'BUILD', 'BUT', 'BUY', 'BY', 'CALL', 'CAME', 'CAN', 'CAR',
+  'CARRY', 'CHANGE', 'CHECK', 'CHILDREN', 'CITY', 'CLEAN', 'CLOSE', 'COLD', 'COME',
+  'COULD', 'COUNTRY', 'CUT', 'DID', 'DIDN’T', 'DO', 'DOES', 'DOG', 'DOING', 'DOLLAR',
+  'DONE', 'DOOR', 'DOWN', 'DRAW', 'DREAM', 'DRINK', 'DRIVE', 'DROP', 'DRY', 'DURING',
   ];
   return dictionary.includes(word.toUpperCase());
 }
